8b428cb547f6bae7ef2dd166a533e22f3f66ca6c
Aung Thiha <55389773+aungthiha-mh@users.noreply.github.com>
2022-12-27 13:56:22 +0630

    Adding link for understanding of VIewModelFactory

--- app/src/main/java/com/flexath/notes/data/ViewModelFactory.kt
+++ app/src/main/java/com/flexath/notes/data/ViewModelFactory.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
+// Should read from this link - https://dev.to/theplebdev/understanding-the-viewmodelproviderfactory-in-android-with-kotlin-11dp
+
 class ViewModelFactory(private val repository: NoteRepository):ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if(modelClass.isAssignableFrom(NoteViewModel::class.java)){
